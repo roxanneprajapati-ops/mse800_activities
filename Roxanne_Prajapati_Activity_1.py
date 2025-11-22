c5fbec98c7f95f12384e780ce7fa71d42c52d414
Roxanne Prajapati <roxanneprajapati@gmail.com>
2025-11-23 09:18:45 +1300

    First Activity

--- Roxanne_Prajapati_Activity_1.py
+++ Roxanne_Prajapati_Activity_1.py
@@ -2,8 +2,15 @@
 Name = "Roxanne Prajapati"
 Age = 28
 Skills = ["Python", "SQL", "PowerBI"]
+Education = ("B.S Electronics and Communications Engineering")
+ContactDetails = {"email": "roxanne@gmail.com", "phone": "123-123-3344"}
+certifications = [{"cert1", "cert2"},{"cert3", "cert4"}]
 
 print(Name)
 print(Age)
 print(Skills)
+print(Education)
+print(ContactDetails)
+print(certifications)
+
 
