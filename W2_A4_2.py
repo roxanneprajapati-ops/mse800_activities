99f50bcb0770f92f39feb5f7bc8d882f4107ab76
roxanneprajapati-ops <roxanneprajapati@gmail.com>
2025-11-30 09:22:26 +1300

    Activity 4.2

---
+++ W2_A4_2.py
@@ -0,0 +1,42 @@
+# ---------------------------------------------------------------
+# TITLE HERE
+# Author: Your Name
+# Description:
+#      
+# ---------------------------------------------------------------
+
+class MathSeries:
+    # @staticmethod
+    def factorial_recursive(self, n):
+        if n < 0:
+            raise ValueError("Factorial is not defined for negative numbers.")
+        if n in (0, 1):
+            return 1
+        return n * self.factorial_recursive(n - 1)
+
+
+
+    # @staticmethod
+    def fibonacci_recursive(self,n):
+        if n < 0:
+            raise ValueError("Fibonacci is not defined for negative numbers.")
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        return self.fibonacci_recursive(n - 1) + self.fibonacci_recursive(n - 2)
+
+    def fibonacci_series(self, n):
+        series = []
+        for i in range(n + 1):
+            series.append(self.fibonacci_recursive(i))
+        return series
+
+
+if __name__ == "__main__":
+    n = int(input("Enter a number: "))
+    math_series = MathSeries()
+    print("Factorial (recursive):", math_series.factorial_recursive(n))
+    print("Fibonacci (recursive):", math_series.fibonacci_series(n))
+
+
