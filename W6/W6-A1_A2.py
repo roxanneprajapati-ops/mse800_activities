63b20f0f8cb95b0f9c8f462398e064ce7d79d280
roxanneprajapati-ops <roxanneprajapati@gmail.com>
2026-01-24 12:21:54 +1300

    Updated code to fulfill the requirements

--- W6/W6-A1_A2.py
+++ W6/W6-A1_A2.py
@@ -1,45 +1,46 @@
-class Student:
-    def __init__(self, student_id, name, score):
-        self.student_id = student_id
-        self.name = name
-        self.score = score
+class StudentDict:
+    def __init__(self, student_names, student_scores):
+        # Dictionaries passed in from main
+        self.student_names = student_names
+        self.student_scores = student_scores
 
-    def has_passed(self, pass_mark=50):
-        return self.score >= pass_mark
+    def get_passed_students(self, pass_mark=50):
+        # Combine the two dictionaries and return only students
+        # with score above 50
+        return {
+            student_id: {"name": name, "score": score}
+            for (student_id, name), score in zip(
+                self.student_names.items(),
+                self.student_scores.values()
+            )
+            if score >= pass_mark
+        }
 
 
 # Main program
 if __name__ == "__main__":
-    # List of student object
-    students = [
-        Student("S001", "Alice", 78),
-        Student("S002", "Bob", 45),
-        Student("S003", "Charlie", 62),
-        Student("S004", "Diana", 49),
-        Student("S005", "Ethan", 85),
-    ]
-
-    # Part 1:
-    # Dictionary {student_id : student_name}
-    id_name_dict = {s.student_id: s.name for s in students}
-    print(id_name_dict)
-
-    # Dictionary {student_id: student_score}
-    id_score_dict = {s.student_id: s.score for s in students}
-    print(id_score_dict)
-
-
-    # combine dictionaries and keep only passed students
-    passed_students = {
-        student_id: {"name": name, "score": score}
-        for (student_id, name), score in zip(
-            id_name_dict.items(),
-            id_score_dict.values()
-        )
-        if score >= 50
+    # Part 1: dictionaries creation
+    student_names = {
+        "S1": "Albert",
+        "S2": "Benj",
+        "S3": "Roxanne",
+        "S4": "Earl",
+        "S5": "Ann"
     }
 
-    # Output
-    print("Students who passed MSE800:")
-    for student_id, info in passed_students.items():
+    student_scores = {
+        "S1": 78,
+        "S2": 45,
+        "S3": 62,
+        "S4": 49,
+        "S5": 85
+    }
+
+    records = StudentDict(student_names, student_scores)
+    
+    # Part 2: combine and filter passed students
+    passed_students = records.get_passed_students()
+    #print(passed_students)
+    print("Top 3 students who passed:")
+    for student_id, info in list(passed_students.items())[:3]:
         print(f"ID: {student_id}, Name: {info['name']}, Score: {info['score']}")
