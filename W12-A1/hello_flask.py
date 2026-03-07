33124bd4315a5f4197c52efd6d074ebb58c03345
roxanneprajapati-ops <roxanneprajapati@gmail.com>
2026-03-08 09:39:02 +1300

    Adding hyperlink and image

--- W12-A1/hello_flask.py
+++ W12-A1/hello_flask.py
@@ -4,19 +4,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_flask():
-    return "<p>Hello Flask Framework!</p>"
+    return "<p>Hello Flask Framework!</p> <p> Please go to <a href='/admin'>Admin</a></p>"
 
 @app.route("/admin")
 def admin():
-    return "<p>Hello Admin!</p>"
+    return "<p>Hello Admin!</p><img src='/static/logo.png'>"
 
 @app.route("/bye")
 def bye():
     return "<p>Bye Flask!</p>"
 
-@app.route("/username/<name>")
-def username(name):
-    return f"Hello {name.capitalize()}"
+@app.route("/username/<name>/<user_id>")
+def username(name, user_id):
+    return f"Hello {name.capitalize()} : {user_id}"
+
+
+
 
 
 if __name__ == "__main__":
