f3691e46c91e3005c27e3cdd3f323d14c21d9eb5
roxanneprajapati-ops <roxanneprajapati@gmail.com>
2026-03-08 10:18:31 +1300

    css activity

--- W12-A1/hello_flask.py
+++ W12-A1/hello_flask.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def hello_flask():
-    return "<p>Hello Flask Framework!</p> <p> Please go to <a href='/admin'>Admin</a></p>"
+    image =''
+    if request.method == "POST":
+        file = request.files["image"]
+        file.save("static/" + file.filename)
+        image = f"<h2>Uploaded Image</h2><img src='/static/{file.filename}' width='300'>"
+
+    return f"""
+        <body style='background:green; color:white'>
+            <p>Hello Flask Framework!</p>
+            <p> Please go to <a href='/admin'>Admin</a></p>
+            <h2>Upload Image</h2>
+            <form method="POST" enctype="multipart/form-data">
+                <input type="file" name="image">
+                <input type="submit">
+            </form>
+            {image}
+        </body>
+        """
+
 
 @app.route("/admin")
 def admin():
